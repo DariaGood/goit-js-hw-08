0a5a380d2a9b0897f02822d79fc7e320a7d2607b
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-03-02 12:38:11 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+//  Імпорт бібліотеки  і стилів
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+
+const galleryGeneral = document.querySelector(".gallery");
+
+
+//Додаємо галерею на сторінку
+const newArrayImages = galleryItems
+  .map(
+    (image) => `<a class="gallery__item" href="${image.original}">
+<img class="gallery__image" 
+src="${image.preview}" 
+alt="${image.description}" />
+</a>`
+  )
+  .join(""); // Дуже важливо!!!
+galleryGeneral.insertAdjacentHTML("afterbegin", newArrayImages);
+
+//Додаємо SimpleLightbox - бібліотеку JS для галереї
+
+let gallery = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
